4d30c040178d7231e970e3a66edef559e5cbceff
petyadev <80815822+petyadev1@users.noreply.github.com>
2022-01-13 17:49:23 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$mail = $_POST['email'];
+$user = $_POST['username'];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css">
+  <link href="https://loginapibtn.followrel.repl.co/style.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+  <div id="gSignInWrapper">
+    <span class="label">Sign in with:</span>
+    <div onclick="goToFRL()" id="customBtn" class="customGPlusSignIn">
+      <span class="icon"><img class="FollowrelImg" src="assets/playstore12.png"></span>
+      <span class="buttonText">Followrel</span>
+    </div>
+  </div>
+  <script>
+    function goToFRL() {
+   window.location.replace("http://followrel.ga/auth.php?app=YOURAPPNAME&uri=YOURAPPURL/this.php");
+    }
+  </script>
+</body>
+</html>
